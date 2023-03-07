0abaa70b81416ebd5e240d4375d263a20823b76a
saka <sshhmn@yahoo.com>
2023-03-07 18:44:21 +0330

    Add project files.

---
+++ WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View(new Quiz());
+        }
+
+        [HttpPost]
+        public IActionResult Register(Quiz quiz)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ WebApplication2/Models/Quiz.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Quiz
+    {
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        [StringLength(11,MinimumLength =11,ErrorMessage = "تعداد رقم ها باید دقیقا 11 رقم باشد")]
+        public string Mobile { get; set; }
+    }
+}
